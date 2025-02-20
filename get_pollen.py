6f6ab8ab808d087eb97729b55106d3774ae1ea22
Minh Bui <buihoangminh00@gmail.com>
2025-02-19 21:47:56 -0600

    add a way to gather pollen data

---
+++ get_pollen.py
@@ -0,0 +1,19 @@
+import sys
+import pprint
+from pollen import pollendata
+
+
+if sys.argv[1] == 'tree':
+    globals()['pollendata']().tree()
+if sys.argv[1] == 'weed':
+    globals()['pollendata']().weed()
+if sys.argv[1] == 'grass':
+    globals()['pollendata']().grass()
+if sys.argv[1] == 'spores':
+    globals()['pollendata']().spores()
+    """print(globals())
+    
+    print(sys.argv[1])"""
+   
+
+#print(pollendata().tree())
